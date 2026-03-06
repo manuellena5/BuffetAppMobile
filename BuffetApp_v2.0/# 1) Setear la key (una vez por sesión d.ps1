74fa4e4c3b665e8fe643bfeb72f2e65a7b15ab75
manuellena5 <manu_landeta@hotmail.com>
2026-03-06 12:59:33 -0300

    Reset SUPABASE_SERVICE_ROLE_KEY for session
    Reset the SUPABASE_SERVICE_ROLE_KEY environment variable.

--- BuffetApp_v2.0/# 1) Setear la key (una vez por sesión d.ps1
+++ BuffetApp_v2.0/# 1) Setear la key (una vez por sesión d.ps1
@@ -1,5 +1,5 @@
 # 1) Setear la key (una vez por sesión de terminal)
-$env:SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uY2VtbmxodGd2dHVidGt2aXZkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTA1OTk3MiwiZXhwIjoyMDc2NjM1OTcyfQ.QCixSuT7-qErBcL0yRNI4ythBptnT4xTKO9g0lP_Zqg"
+$env:SUPABASE_SERVICE_ROLE_KEY = ""
 
 # 2) Compilar + subir + generar metadata
 .\tools\deploy-apk.ps1
